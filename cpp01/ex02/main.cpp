339df2dde6d8a3e77a092900483c77d63a9f14ac
Andranik Adyan <andranikadyan@gmail.com>
2025-08-25 21:56:12 +0400

    add ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string& stringREF = str;
+
+	
+
+	return 0;
+}
