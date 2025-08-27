b4cc0da7baafc717a7a16011cb60a377cf3930a8
AndranikAdyan <andranikadyan@gmail.com>
2025-08-27 14:50:27 +0400

    push ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -7,7 +7,13 @@ int main()
 	std::string *stringPTR = &str;
 	std::string& stringREF = str;
 
-	
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "The value of the string variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF;
 
 	return 0;
 }
