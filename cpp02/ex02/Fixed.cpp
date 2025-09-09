c78002cc345b1eb5175a5571572ba63a9180eb9c
AndranikAdyan <andranikadyan@gmail.com>
2025-09-09 23:06:18 +0400

    fix by subject cpp02 - ex02

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -1,41 +1,33 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() {
-	std::cout << "Default constructor called" << std::endl;
 	this->_num = 0;
 }
 
 Fixed::Fixed(const int num) {
-	std::cout << "Int constructor called" << std::endl;
 	this->_num = num << Fixed::_fractionalBits;
 }
 
 Fixed::Fixed(const float fnum) {
-	std::cout << "Float constructor called" << std::endl;
 	this->_num = (int)roundf(fnum  * (1 << Fixed::_fractionalBits));
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& other) {
-	std::cout << "Copy constructor called" << std::endl;
 	this->_num = other._num;
 }
 
 void Fixed::setRawBits(int const raw) {
-	std::cout << "setRawBits member function called" << std::endl;
 	this->_num = raw;
 }
 
 int Fixed::getRawBits() const {
-	std::cout << "getRawBits member function called" << std::endl;
 	return this->_num;
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this == &other)
 		return *this;
 	this->_num = other._num;
