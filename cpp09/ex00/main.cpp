83cb847b91848c049a88dea5005f17a59c57c469
AndranikAdyan <117289402+AndranikAdyan@users.noreply.github.com>
2026-03-14 07:20:27 +0400

    Implement error handling in main function
    Added error handling for file operations and wrapped main logic in a try-catch block.

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -15,24 +15,29 @@ void printMap(const std::map<K, V>& data)
                   << it->second << std::endl;
     }
 }
-// ?????????????????????????????????????????????
+
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        throw std::invalid_argument("could not open file");
+    try {
+        if (argc != 2)
+            throw std::invalid_argument("could not open file");
+            
+        std::ifstream db("data.txt");
+        if (!db.is_open())
+            throw std::runtime_error("could not open database");
+        
+        BitcoinExchange be;
+        be.fill_db(db);
+        
+        std::ifstream input(argv[1]);
+        if (!input.is_open())
+            throw std::runtime_error("could not open database");
     
-    std::ifstream db("data.txt");
-    if (!db.is_open())
-        throw std::runtime_error("could not open database");
-    
-    BitcoinExchange be;
-    be.fill_db(db);
-    
-    std::ifstream input(argv[1]);
-    if (!input.is_open())
-        throw std::runtime_error("could not open database");
-
-    be.print_result(input);
-    return 0;
+        be.print_result(input);  
+        return 0;
+    }
+    catch(const std::exception& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+        return 1;
+    }
 }
-
